c2b203138f4ebbd0ca1ef324a7bc1e87ba93eae5
webababy <feiyeyuji@gmail.com>
2019-03-12 00:24:39 +1100

    Updates

---
+++ precache-manifest.7d2b97714be9a2b31b77a18eeb54de3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10e06ced695b00e56c6d",
+    "url": "/saasmodulesite/static/js/runtime~main.10e06ced.js"
+  },
+  {
+    "revision": "f6e01865b10782f90dee",
+    "url": "/saasmodulesite/static/js/main.f6e01865.chunk.js"
+  },
+  {
+    "revision": "258969de8db6ae094015",
+    "url": "/saasmodulesite/static/js/1.258969de.chunk.js"
+  },
+  {
+    "revision": "f6e01865b10782f90dee",
+    "url": "/saasmodulesite/static/css/main.590fd2c1.chunk.css"
+  },
+  {
+    "revision": "258969de8db6ae094015",
+    "url": "/saasmodulesite/static/css/1.cb2980c2.chunk.css"
+  },
+  {
+    "revision": "7cbe80f504703155f0ab38af2ac9110c",
+    "url": "/saasmodulesite/index.html"
+  }
+];
